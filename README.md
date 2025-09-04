58ccfc18cc73a1573c7cb54c8ca6fda40fec9c76
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2025-09-04 11:02:14 +0200

    Lecture 6 - Rabbit.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 2025 - PG3402 - Microservices
 
-
+git 
 
 Card SVGs from:
 https://code.google.com/archive/p/vector-playing-cards/
---
+++ resolver/src/main/java/org/cards/resolver/services/ResolverService.java
@@ -0,0 +1,7 @@
+package org.cards.resolver.services;
+
+import org.cards.resolver.model.ResolverRound;
+
+public interface ResolverService {
+    public String resolveRound(ResolverRound round);
+}
