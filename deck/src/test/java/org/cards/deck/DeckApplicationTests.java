7553169397bb1818037a638b7bdcd27f5656553f
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2025-10-07 16:10:46 +0200

    Lecture 15 - A test, setting up for profiles.

--- deck/src/test/java/org/cards/deck/DeckApplicationTests.java
+++ deck/src/test/java/org/cards/deck/DeckApplicationTests.java
@@ -1,25 +1,23 @@
-package org.cards.resolver;
+package org.cards.deck;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.Matchers.*;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-@Transactional
-class ResolverApplicationTests {
+class DeckApplicationTests {
 
     @Autowired
     private MockMvc mockMvc;
@@ -30,11 +28,10 @@ class ResolverApplicationTests {
 
     @Test
     void sizeTest() throws Exception {
-        mockMvc.perform(get("/api/resolver/outcomes")
-                .contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/api/deck")
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.*", hasSize(0)));
+                .andExpect(jsonPath("$.*", hasSize(7)));
     }
-
 }
